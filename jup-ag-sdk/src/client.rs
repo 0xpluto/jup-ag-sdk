7e354d2d4121bcec3a0b62d7e8580096f0b2211c
0xpluto <jackson2020@me.com>
2025-07-21 09:10:32 -0400

    remove reqwest feature and add clone derive

--- jup-ag-sdk/src/client.rs
+++ jup-ag-sdk/src/client.rs
@@ -5,7 +5,7 @@ use reqwest::{
 
 /// `JupiterClient` is a client wrapper to interact with the Jupiter Aggregator APIs.
 /// It is your gateway to interact with the Jupiter exchange API
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct JupiterClient {
     pub client: Client,
     pub base_url: String,
